7c5a085010505910d48df2302ff722a53c3bc36e
Leon <ali.rostami33@gmail.com>
2020-10-06 12:59:36 +0330

    First time commit.

---
+++ app/src/main/java/com/leon/reading_counter/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.leon.reading_counter.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.leon.reading_counter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
