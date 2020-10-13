1f55f2a2a1afad7ce26947a4deeab31b929f48db
Leon <ali.rostami33@gmail.com>
2020-10-13 15:02:33 +0330

    First vc commit.

--- app/src/main/java/com/leon/reading_counter/activities/MainActivity.java
+++ app/src/main/java/com/leon/reading_counter/activities/MainActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.leon.reading_counter.R;
+import com.leon.reading_counter.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
