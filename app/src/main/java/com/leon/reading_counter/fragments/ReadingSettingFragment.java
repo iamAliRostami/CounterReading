3dc24dbbd29ccc55c850483622ea83e294a1d734
Leon <ali.rostami33@gmail.com>
2020-10-24 11:53:19 +0330

    first ui commit.

---
+++ app/src/main/java/com/leon/reading_counter/fragments/ReadingSettingFragment.java
@@ -0,0 +1,40 @@
+package com.leon.reading_counter.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.leon.reading_counter.databinding.FragmentReadingSettingBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ReadingSettingFragment extends Fragment {
+    FragmentReadingSettingBinding binding;
+
+    public ReadingSettingFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentReadingSettingBinding.inflate(inflater, container, false);
+        initialize();
+        return binding.getRoot();
+    }
+
+    void initialize() {
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
