f3f8a8c72ec79224271e8bd2c4997d69a94cc346
Leon <ali.rostami33@gmail.com>
2020-12-09 17:02:31 +0330

    reading configuration

--- app/src/main/java/com/leon/counter_reading/utils/GPSTrackerOld.java
+++ app/src/main/java/com/leon/counter_reading/utils/GPSTrackerOld.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,12 +14,9 @@ import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.leon.counter_reading.MyApplication;
 import com.leon.counter_reading.R;
 import com.leon.counter_reading.tables.SavedLocations;
@@ -29,46 +25,21 @@ import org.osmdroid.config.Configuration;
 
 import java.util.ArrayList;
 
-public class GPSTracker extends Service {
+public class GPSTrackerOld extends Service {
     final Activity activity;
     boolean canGetLocation = false;
     double latitude;
     double longitude;
     boolean checkGPS = false;
     boolean checkNetwork = false;
-    ArrayList<SavedLocations> savedLocations = new ArrayList<>();
     Location location;
     LocationManager locationManager;
-    FusedLocationProviderClient fusedLocationClient;
     LocationRequest locationRequest;
-    LocationCallback locationCallback = new LocationCallback() {
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            if (locationResult == null) {
-                return;
-            }
-            for (Location location : locationResult.getLocations()) {
-                if (location != null) {
-                    latitude = location.getLatitude();
-                    longitude = location.getLongitude();
-                    savedLocations.add(new SavedLocations(location.getAccuracy(), longitude, latitude));
-                    Log.e("accuracy 3", String.valueOf(location.getAccuracy()));
-                }
-            }
-        }
-    };
-    OnSuccessListener<Location> onSuccessListener = new OnSuccessListener<Location>() {
-        @Override
-        public void onSuccess(Location location) {
-            if (location != null) {
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
-                savedLocations.add(new SavedLocations(location.getAccuracy(), longitude, latitude));
-                Log.e("accuracy 2", String.valueOf(location.getAccuracy()));
-            }
-        }
-    };
-    LocationListener locationListener = new LocationListener() {
+
+    GoogleApiClient googleApiClient;
+
+    ArrayList<SavedLocations> savedLocations = new ArrayList<>();
+    android.location.LocationListener locationListener = new android.location.LocationListener() {
         public void onLocationChanged(Location location) {
             if (locationManager != null)
                 locationManager.removeUpdates(locationListener);
@@ -89,8 +60,28 @@ public class GPSTracker extends Service {
         public void onProviderDisabled(String provider) {
         }
     };
+    com.google.android.gms.location.LocationListener locationListenerGoogle =
+            new com.google.android.gms.location.LocationListener() {
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        savedLocations.add(new SavedLocations(location.getAccuracy(), longitude, latitude));
+                        Log.e("accuracy 2", String.valueOf(location.getAccuracy()));
+                    }
+                }
 
-    public GPSTracker(Activity activity) {
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                public void onProviderEnabled(String provider) {
+                }
+
+                public void onProviderDisabled(String provider) {
+                }
+            };
+
+    public GPSTrackerOld(Activity activity) {
         this.activity = activity;
         Configuration.getInstance().load(activity,
                 PreferenceManager.getDefaultSharedPreferences(activity));
@@ -99,6 +90,18 @@ public class GPSTracker extends Service {
         } else {
             getLocation();
         }
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public boolean canGetLocation() {
+        return this.canGetLocation;
     }
 
     @SuppressLint("MissingPermission")
@@ -153,6 +156,18 @@ public class GPSTracker extends Service {
         new Handler().postDelayed(this::getLocation, MyApplication.MIN_TIME_BW_UPDATES);
     }
 
+    public void stopListener() {
+        if (locationManager != null)
+            locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        stopFusedLocation();
+        stopListener();
+        return null;
+    }
+
     boolean checkGooglePlayServices() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         CustomToast customToast = new CustomToast();
@@ -171,47 +186,52 @@ public class GPSTracker extends Service {
     }
 
     void startFusedLocation() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(activity).addApi(LocationServices.API)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnectionSuspended(int cause) {
+                        }
+
+                        @Override
+                        public void onConnected(Bundle connectionHint) {
+
+                        }
+                    }).addOnConnectionFailedListener(result -> {
+
+                    }).build();
+        }
+        googleApiClient.connect();
+        registerRequestUpdateGoogle(locationListenerGoogle);
+    }
+
+    void registerRequestUpdateGoogle(final com.google.android.gms.location.LocationListener listener) {
         locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(MyApplication.MIN_TIME_BW_UPDATES);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
-        registerRequestUpdateGoogle();
+        new Handler().postDelayed(() -> {
+            try {
+                LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,
+                        locationRequest, listener);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (!isGoogleApiClientConnected()) {
+                    googleApiClient.connect();
+                }
+                registerRequestUpdateGoogle(listener);
+            }
+        }, MyApplication.MIN_TIME_BW_UPDATES);
     }
 
-    @SuppressLint("MissingPermission")
-    void registerRequestUpdateGoogle() {
-        fusedLocationClient.getLastLocation().addOnSuccessListener(activity,
-                onSuccessListener);
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public boolean canGetLocation() {
-        return this.canGetLocation;
+    boolean isGoogleApiClientConnected() {
+        return googleApiClient != null && googleApiClient.isConnected();
     }
 
     void stopFusedLocation() {
-        if (fusedLocationClient != null) {
-            fusedLocationClient.removeLocationUpdates(locationCallback);
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
         }
-    }
-
-    public void stopListener() {
-        if (locationManager != null)
-            locationManager.removeUpdates(locationListener);
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        stopFusedLocation();
-        stopListener();
-        return null;
     }
 }
